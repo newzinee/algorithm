22af628372cdbaf76984fb6f687ed4d10524ad7f
treasureBear94 <qvo7896@gmail.com>
2018-04-04 18:17:01 +0900

    별찍기3

---
+++ BOJAlgorithm/src/bojQ2440/Main.java
@@ -0,0 +1,26 @@
+package bojQ2440;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/2440
+//별찍기3
+//5
+//*****
+//****
+//***
+//**
+//*
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i=n; i>0;i--) {
+			for(int s=i; s>0; s--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
