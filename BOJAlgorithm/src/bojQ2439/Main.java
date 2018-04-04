62fe367030868719faee7fff7e644ac8be137cfe
treasureBear94 <qvo7896@gmail.com>
2018-04-04 18:07:51 +0900

    Q2439 별찍기2

---
+++ BOJAlgorithm/src/bojQ2439/Main.java
@@ -0,0 +1,32 @@
+package bojQ2439;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/2439
+//별찍기 2
+// 5
+//    *
+//   **
+//  ***
+// ****
+//*****
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();	// 1<=n<=10
+		sc.close();
+		
+		for(int i=n; i>0; i--) {
+			int j=(i-1);
+			for(int m=j;m>0;m--) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<(n-j);k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
